380f765a04df6749fc7e7144d90135bad01450e2
Zhivko <zivkostoimcev1551@outlook.com>
2025-03-27 01:04:41 +0100

    [INITIAL]

---
+++ src/main/java/dev/personal/planner/Entity/UserController.java
@@ -0,0 +1,24 @@
+package dev.personal.planner.Entity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<Users> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
